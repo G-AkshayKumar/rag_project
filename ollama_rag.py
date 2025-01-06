d6481112ce7edc15ea7a451a7d25ea4ac2d74b97
G-AkshayKumar <124762545+G-AkshayKumar@users.noreply.github.com>
2025-01-06 14:15:21 +0530

    Update ollama_rag.py

--- ollama_rag.py
+++ ollama_rag.py
@@ -15,7 +15,9 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain.retrievers import BM25Retriever, EnsembleRetriever
 from torch import cuda
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 device = "cuda" if cuda.is_available() else "cpu"
 
 st.set_page_config(page_title="Chatbot (Folder Version)")
