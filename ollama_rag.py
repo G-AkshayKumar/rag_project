2ea28c7b4c457b791f8ec1bb52897d61703e97ef
G-AkshayKumar <124762545+G-AkshayKumar@users.noreply.github.com>
2025-01-06 16:36:16 +0530

    Update ollama_rag.py

--- ollama_rag.py
+++ ollama_rag.py
@@ -28,7 +28,7 @@ class ChatPDF:
         self.vector_db = None
         self.llm = Ollama(model="""mistral""", 
                           temperature=0.2,
-                          base_url="http://localhost:11434")
+                          base_url="http://localhost:11434/api/generate")
         self.chain = None
         self.processed_files = []  # Track processed files
 
